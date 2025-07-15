fa814bb918fcbc71dae078b1ee2409bcb380af2a
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-07-15 10:27:26 +0530

    created folder structure

---
+++ src/com/school/Main.java
@@ -0,0 +1,8 @@
+package com.school;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Attendance Management System");
+    }
+}
