072e45c2d39ce85cf8a519f3466ba924de17b86e
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-07-15 10:37:51 +0530

    Welcome program

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,7 +2,7 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Attendance Management System");
+        System.out.println("Welcome to the School Attendance System Project!");
+        System.out.println("Session 1: Project Setup and Orientation Complete.");
     }
 }
